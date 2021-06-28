e46c0fde5b3ba64156e5c88430ccdf24acef78f9
aufalmarom <aufal.arrozi@bankmandiri.co.id>
2021-06-28 17:17:03 +0700

    dynamic and result question

--- src/app/dynamic-question/dynamic-question.component.ts
+++ src/app/dynamic-question/dynamic-question.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dynamic-question',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dynamic-question.component.css']
 })
 export class DynamicQuestionComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
+  createForm() {
+    this.form = new FormGroup({
+      'username': new FormControl(null, [Validators.required, Validators.min(1), Validators.max(15)]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'fullname': new FormControl(null, [Validators.required, Validators.min(1), Validators.max(15)]),
+      'age': new FormControl(null, [Validators.required, Validators.min(1), Validators.max(200)]),
+      'sex': new FormControl(null, [Validators.required]),
+  });
+ }
+ 
   ngOnInit() {
+    this.createForm();
+  }
+  
+  onSubmit(data){
+    localStorage.setItem('data', JSON.stringify(data));
+    this.router.navigate(['/result-question']);
   }
 
 }
